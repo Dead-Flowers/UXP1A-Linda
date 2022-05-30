b3dbcaeac06352f9ef4351f623dac5b0f460514c
lorlen <filip.lew975@gmail.com>
2022-05-31 00:14:04 +0200

    Preliminary work on CLI

--- src/test-host/main.cpp
+++ src/test-host/main.cpp
@@ -1,6 +1,12 @@
+#include <argparse/argparse.hpp>
+
 #include "linda/linda.h"
 
-int main() {
+int main(int argc, char** argv) {
+    auto parser = argparse::ArgumentParser("linda-server");
+    parser.add_description("Hosts the global Linda tuple space.");
+    parser.parse_args(argc, argv);
+
     TupleSpaceHost host;
     host.init("./key.k", 2137);
     host.runServer();
