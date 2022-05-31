0c7f5031634d1bb955da404b174ea041fb2b0b93
tedtheripper <lemon.juice0731@gmail.com>
2022-06-01 01:43:45 +0200

    refactored lexer and added new tests

--- lib/parser/test/patternParserTest.cpp
+++ lib/parser/test/patternParserTest.cpp
@@ -183,3 +183,35 @@ TEST_CASE("Test failed invalid combination") {
     PatternParser p(lexer);
     CHECK_THROWS_AS(p.parse(), PatternParsingException);
 }
+
+TEST_CASE("Test parsing negative integer pattern") {
+    auto patternInput = "(integer:<-5)";
+    Lexer lexer(patternInput);
+    PatternParser p(lexer);
+    auto res = p.parse();
+
+    CHECK(res.size() == 1);
+    CHECK(res[0].type == TupleDataType::Integer);
+    CHECK(res[0].op == TupleOperator::Less);
+    CHECK(std::get<int64_t>(res[0].value.value()) == -5);
+}
+
+TEST_CASE("Test parsing multiple patterns") {
+    auto patternInput = "(integer:>0, float:<10.5, string:*)";
+    Lexer lexer(patternInput);
+    PatternParser p(lexer);
+    auto res = p.parse();
+
+    CHECK(res.size() == 3);
+    CHECK(res[0].type == TupleDataType::Integer);
+    CHECK(res[0].op == TupleOperator::Greater);
+    CHECK(std::get<int64_t>(res[0].value.value()) == 0);
+
+    CHECK(res[1].type == TupleDataType::Float);
+    CHECK(res[1].op == TupleOperator::Less);
+    CHECK(std::abs(std::get<float>(res[1].value.value()) - 10.5) < 1e-5);
+
+    CHECK(res[2].type == TupleDataType::String);
+    CHECK(res[2].op == TupleOperator::Equal);
+    CHECK(res[2].value == std::nullopt);
+}
