1390f5567838a53d8337e0ecec2647cfeea897b5
lorlen <filip.lew975@gmail.com>
2022-06-01 14:31:06 +0200

    add server arguments

--- src/linda-server/main.cpp
+++ src/linda-server/main.cpp
@@ -5,11 +5,18 @@
 int main(int argc, char** argv) {
     auto parser = argparse::ArgumentParser("linda-server");
     parser.add_description("Hosts the global Linda tuple space.");
+    parser.add_argument("-k", "--key-path")
+        .help("path to be used as a key to identify the Linda Host queue")
+        .default_value(std::string("/tmp/linda.key"));
+    parser.add_argument("-p", "--project-id")
+        .help("project ID to be used along with --key-path to identify the Linda Host queue")
+        .scan<'i', int>()
+        .default_value((int) 1);
     parser.parse_args(argc, argv);
 
     TupleSpaceHost host;
-    host.init("./key.k", 2137);
+    host.init(parser.get<std::string>("key-path").c_str(), parser.get<int>("project-id"));
     host.runServer();
-    puts("done\n");
+
     return 0;
 }
