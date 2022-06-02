6a1c19378bddfaa987abd36f619e4584fa9e3da1
tedtheripper <lemon.juice0731@gmail.com>
2022-06-02 20:19:30 +0200

    added some acc tests

--- acceptance_tests/test.sh
+++ acceptance_tests/test.sh
@@ -4,8 +4,8 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 BASE_PATH="$SCRIPTPATH/../cmake-build-debug/src"
-CLIENT_PATH="$BASE_PATH/client"
-HOST_PATH="$BASE_PATH/server"
+CLIENT_PATH="$BASE_PATH/linda-client"
+HOST_PATH="$BASE_PATH/linda-server"
 
 runTest () {
   echo "Running test $1"
@@ -24,4 +24,10 @@ runTest () {
   echo ""
 }
 
-runTest "Something" 0 "./1/test_1"
+runTest "Input integer" 0 "./1/test_1"
+runTest "Read float" 0 "./2/test_2"
+runTest "Read multiple tuples" 0 "./3/test_3"
+runTest "Read after input" 0 "./4/test_4"
+runTest "Consume with string pattern" 0 "./5/test_5"
+runTest "Too big tuples" 0 "./6/test_6"
+runTest "Multiple clients test" 0 "./7/test_7a" 1 "./7/test_7b" 1 "./7/test_7c" 1 "./7/test_7d" 1 "./7/test_7e" 1 "./7/test_7f"
