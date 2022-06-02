0a58aea0a3eddd449c870463b63f0a6d46821b15
PowerOfDark <przem2003@o2.pl>
2022-06-02 19:20:24 +0300

    Acceptance tests

--- acceptance_tests/runner.sh
+++ acceptance_tests/runner.sh
@@ -5,19 +5,23 @@ shift 2
 CLIENT_PIDS=""
 HOST_PID=""
 
-nohup sh -c "$HOST_PATH" & HOST_PID="$!" > /dev/null
+bash -c "$HOST_PATH -r" >/dev/null 2>&1 &
+HOST_PID="$!"
 
 while (( "$#" >= 2 )); do
-    truncate "$2.out" --size 0
-    nohup bash -c '
-    while read line; do
-      echo "$line" | xargs bash -c "$0" >> "$1"
-      echo "Exit code: $?" >> "$1"
-    done' "$CLIENT_PATH" "$2.out" < "$2" & CLIENT_PIDS="CLIENT_PIDS $!"
-
     sleep "$1"
+    truncate "$2.out" --size 0
+    bash -c '
+    cat "$1" | while read line;
+    do
+      echo "$line";
+      bash -c "$0 $line" >> "$1.out";
+      echo "Exit code: $?" >> "$1.out";
+    done' "$CLIENT_PATH" "$2" >/dev/null 2>&1 &
+    CLIENT_PIDS="$CLIENT_PIDS $!"
     shift 2
 done
 
 wait $CLIENT_PIDS
-kill -9 $HOST_PID
+kill "$HOST_PID"
+echo "Done"
--- acceptance_tests/test.sh
+++ acceptance_tests/test.sh
@@ -4,7 +4,24 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 BASE_PATH="$SCRIPTPATH/../cmake-build-debug/src"
-CLIENT_PATH="$BASE_PATH/linda-client"
-HOST_PATH="$BASE_PATH/linda-server"
+CLIENT_PATH="$BASE_PATH/client"
+HOST_PATH="$BASE_PATH/server"
 
-bash runner.sh "$HOST_PATH" "$CLIENT_PATH" 0 "./1/1.in"
+runTest () {
+  echo "Running test $1"
+  shift
+  bash runner.sh "$HOST_PATH" "$CLIENT_PATH" "$@"
+  while (( "$#" >= 2 )); do
+    if [[ -f "$2.expected" ]]
+    then
+      diff -s "$2.expected" "$2.out" || exit 1
+    else
+      echo "Creating expected file"
+      cp "$2.out" "$2.expected"
+    fi
+    shift 2
+  done
+  echo ""
+}
+
+runTest "Something" 0 "./1/test_1"
